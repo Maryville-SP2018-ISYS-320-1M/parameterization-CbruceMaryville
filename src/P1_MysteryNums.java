da04a55db010a2cfeafd8aae717fc4ccaf9244f8
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-25 21:26:33 -0500

    Update P1_MysteryNums.java

--- src/P1_MysteryNums.java
+++ src/P1_MysteryNums.java
@@ -1,23 +1,34 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 3/25/2018
 */
 
 /*
  1. Your output prediction:
-  
+  I would say that it prints a 15, 42 
+  then it will print then 10, (15+10) which would be 10, 25
   
  */
 
 /*
  3. Were you correct? Explain if you were not (what did you learn?)
- 
+ I was correct! 
  
  */
 
 public class P1_MysteryNums {
 
-
+    public static void main(String[] args) {
+        int x = 15;
+        sentence( x, 42 );
+        
+        int y = x - 5;
+        sentence( y, x + y);
+    }
+    
+    public static void sentence( int num1, int num2 ) {
+        System.out.println( num1 + " " + num2 );
+    }
 
 }
