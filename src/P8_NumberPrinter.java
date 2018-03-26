ed6dcf5f42e523b9a7edad308cda9072ae2a5826
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-25 21:44:54 -0500

    Update P8_NumberPrinter.java

--- src/P8_NumberPrinter.java
+++ src/P8_NumberPrinter.java
@@ -1,14 +1,39 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 3/25/2018
 */
 
 public class P8_NumberPrinter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PrintNums(5);
+		System.out.println();
+		PrintNumsTwo(5,2);
 	}
 
+	public static void PrintNums(int nums) {
+		nums = (nums+1);
+		for (int i = 1; i < nums; i++) {
+			System.out.print("["+i+"] ");
+		}
+	}
+
+
+//PrintNums but with a second parameter that counts by that many per
+	public static void PrintNumsTwo(int nums, int increment) {
+		nums = (nums+1);
+		for (int i = 1; i < nums; i += increment) {
+			System.out.print("["+i+"] ");
+		}
+	}	
+
 }
+
+
+/*
+OUTPUT
+[1] [2] [3] [4] [5] 
+[1] [3] [5] 
+
+*/
